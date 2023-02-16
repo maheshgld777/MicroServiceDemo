b40a3c6ba34c41103cc46009bec6d9d67780e4a1
Mahesh Pattar <maheshgld777@gmail.com>
2023-02-16 17:29:37 +0530

    Microservice and Ocelot API GW

---
+++ UserService/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UserService.Database;
+using UserService.Database.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UserService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        DatabaseContext dbContext;
+        public UserController()
+        {
+            dbContext = new DatabaseContext();
+        }
+
+        // GET: api/<UserController>
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return dbContext.Users.ToList();
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            return dbContext.Users.Find(id);
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public IActionResult Post([FromBody] User user)
+        {
+            try
+            {
+                dbContext.Users.Add(user);
+                dbContext.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created, user);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+            }
+        }
+    }
+}
---
+++ UserService/Database/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserService.Database.Entities;
+
+namespace UserService.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-9U8FRGU\SQLEXPRESS;Initial Catalog=UserMicroService;" +
+                "Integrated Security=True;TrustServerCertificate=True");
+        }
+    }
+}
